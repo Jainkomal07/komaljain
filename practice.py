363b996196bf969d1339f9df8bb277fc8220950f
Komal Jain <komaljain0798@gmail.com>
2022-12-13 22:05:00 +0530

    time and date

--- practice.py
+++ practice.py
@@ -1,1 +1,15 @@
-123
+import time #first we will import time package
+
+def countdown(t): #the with the help of def function 
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(mins,secs)
+        print(timer, end="\n")
+        time.sleep(1)
+        t -= 1
+
+    print('Timer completed!')
+
+t = input('Enter the time in seconds: ')
+
+countdown(int(t))
